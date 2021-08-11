1a000acfebef57495b0501b7a8ae1db0669cac50
mascDriver <diogoblibi@hotmail.com>
2021-08-11 14:48:42 -0300

    finalizando activity após criar e executando on resume ao inves de sempre onCreate

--- app/src/main/java/br/com/twa/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/twa/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.twa.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,16 +24,30 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
 
+
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View view) {
+                startActivity(new Intent(ListaAlunosActivity.this,
+                        FormularioAlunoActivity.class));
+            }
+        });
+    }
+    @Override
+
+    protected void onResume() {
+        super.onResume();
+
         AlunoDAO dao = new AlunoDAO();
 
-
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Diogo"));
         ListView listaDeAlunos = findViewById(R.id.activity_lista_de_alunos_listview);
-        listaDeAlunos.setAdapter(
-                new ArrayAdapter<>(
-                        this,
-                        android.R.layout.simple_list_item_1, dao.todos()
-                )
-        );
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dao.todos()));
+
     }
 }
